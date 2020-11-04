b9243141d1663ad8a6c296a52e66f2b83878b697
Navyashree H C <navyashree@redintegro.com>
2020-11-04 16:01:27 +0530

    Rest api dynamic content crud operation creation and insertion

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.hello_world, name='hello_world'),
     path('users', views.user_list, name='user_list'),
     path('user/<int:id>', views.user_detail, name='user_detail'),
-    path('user', views.insert_user, name='insert_user')
+    path('user', views.insert_user, name='insert_user'),
+    path('blogs', views.blog_list, name='blog_list'),
+    path('blog', views.insert_blog, name = "insert_blog"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,14 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 import json
+from .models import Blog
+from django.core import serializers
+from django.utils import timezone
 
 def hello_world(request):
     return HttpResponse("Hello, world.")
 
+#static content
 def user_list(request):
     data = [
         {'name': 'Kiran', 'email': 'kiran@redintegro.com'},
@@ -22,4 +26,27 @@ def user_detail(request, id):
 @require_http_methods(["POST"])
 def insert_user(request):
     data = json.loads(request.body.decode('utf-8'))
+    return JsonResponse(data, safe=False)
+
+#Dynamic content Crud operations
+
+#Creation
+def blog_list(request):
+    data = Blog.objects.all()
+    response = serializers.serialize("json", data)
+    return HttpResponse(response, content_type='application/json')
+
+#Insertion
+@csrf_exempt
+@require_http_methods(["POST"])
+def insert_blog(request):
+    data = json.loads(request.body.decode('utf-8'))
+
+    new_blog = Blog(
+        name = data['name'],
+        posted_by = data['postedBy'],
+        posted_on = timezone.now(),
+    )
+
+    new_blog.save()
     return JsonResponse(data, safe=False)
