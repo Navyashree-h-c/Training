20db83121d27494d2b87bde4c745fa5857e05c90
Navyashree H C <navyashree@redintegro.com>
2020-11-04 16:54:54 +0530

    GraphQL app level setup

--- training/schema.py
+++ training/schema.py
@@ -1,6 +1,7 @@
 import graphene
+import blog.schema.schema as Blog
 
-class Query(graphene.ObjectType):
+class Query(Blog.Query,graphene.ObjectType):
     hello = graphene.String(default_value="Hi!")
-    
+
 schema = graphene.Schema(query=Query)
