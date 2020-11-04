3c6e11acc0923339477941a67a7965c6e9c226a2
Navyashree H C <navyashree@redintegro.com>
2020-11-04 15:38:23 +0530

    Blog model created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    name = models.TextField()
+    posted_by = models.TextField()
+    posted_on = models.DateTimeField()
